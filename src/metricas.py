6ddec2f28f032550cb71640028d78558bf6184eb
sofimorenoc <smorenocoppaoliver@udesa.edu.ar>
2026-04-15 17:48:11 -0300

    metricas.py

--- src/metricas.py
+++ src/metricas.py
@@ -9,12 +9,21 @@ def calcular_metricas(datos):
     
     '''
     Calcula metricas del sistema.
-    
     Parametros:
-        - datos: lista de datos procesados.
-        
+        - datos: lista de datos procesados.   
     Retorna:
-        -dict: resultados.
-        
+        -dict: resultados.  
     '''
-    return {}
+
+    total_tiempo = 0
+    total_uso = 0
+    cantidad_registros = len(datos)
+
+    for registro in datos:
+        total_tiempo += registro["tiempo_uso"]
+        total_uso += registro["cantidad_uso"]
+
+    promedio_tiempo = total_tiempo / cantidad_registros
+    promedio_uso = total_uso / cantidad_registros
+
+    return {"promedio_tiempo_uso": promedio_tiempo, "promedio_cantidad_uso": promedio_uso}
