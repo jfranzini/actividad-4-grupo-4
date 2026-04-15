21881a9040d411b6f50b3c8b49a177f2fb17c6a4
sofimorenoc <smorenocoppaoliver@udesa.edu.ar>
2026-04-15 17:56:37 -0300

    Update procesamiento_datos.py

--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -17,5 +17,29 @@ def preparar_datos(datos):
         - list: datos procesados.
         
     '''
-    
+    def preparar_datos(datos):
+
+    '''
+    Prepara los datos para el análisis.
+
+    Parametros:
+        - datos: lista de registros válidos.
+
+    Retorna:
+        - list: datos procesados.
+    '''
+
+    datos_procesados = []
+
+    for registro in datos:
+        nuevo_registro = {
+            "id_participante": registro["id_participante"],
+            "fecha": registro["fecha"],
+            "app": registro["app"].strip().lower(),
+            "cantidad_uso": registro["cantidad_uso"],
+            "tiempo_uso": registro["tiempo_uso"]}
+
+        datos_procesados.append(nuevo_registro)
+
+    return datos_procesados
     return datos
