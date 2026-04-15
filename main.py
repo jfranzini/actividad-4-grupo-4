d359f911f2d084eab0f9f449f6e38d8514a2ecc8
sofimorenoc <smorenocoppaoliver@udesa.edu.ar>
2026-04-15 18:25:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ Created on Wed Apr 15 15:11:11 2026
 
 @author: bausi
 """
-
 from src.carga_datos import cargar_datos
 from src.validacion_datos import filtrar_registros_validos
 from src.procesamiento_datos import preparar_datos
@@ -20,7 +19,7 @@ def main():
         resultados = calcular_metricas(datos_procesados)
         
         print("Resultados:", resultados)
-        
+    
     except Exception as error: 
         print("Error critico en main: ", error)
         
